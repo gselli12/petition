0d97b36697dfb6e3c690047d0924bbb3a7df27aa
gselli12 <tgsellmann@gmail.com>
2017-08-17 18:48:21 +0200

    added seperate file for middleware

---
+++ middleware.js
@@ -0,0 +1,29 @@
+//REQUIRING
+var express = require('express');
+var cookieSession = require('cookie-session');
+const secrets = require("./secrets.json");
+
+//MIDDLEWARE
+
+module.exports = (app) => {
+    app.use(express.static(__dirname + "/public"));
+
+    app.use(require("cookie-parser")());
+
+    app.use(require("body-parser").urlencoded({
+        extended: false
+    }));
+
+    app.use(cookieSession({
+        secret: secrets.secret,
+        maxAge: 1000 * 60 * 60 * 24 * 14
+    }));
+
+    app.use((req, res, next) => {
+        if (req.session.sigId && req.url == "/petition") {
+            res.redirect("/petition/signed");
+        } else {
+            next();
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
+const middleware = require("./middleware.js");
+
 const {addSignature, countRows, getNames} = require("./dbqueries.js");
 var cookieSession = require('cookie-session');
 const secrets = require("./secrets.json");
@@ -11,26 +13,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //MIDDLEWARE
-app.use(express.static(__dirname + "/public"));
-
-app.use(require("cookie-parser")());
-
-app.use(require("body-parser").urlencoded({
-    extended: false
-}));
-
-app.use(cookieSession({
-    secret: secrets.secret,
-    maxAge: 1000 * 60 * 60 * 24 * 14
-}));
-
-app.use((req, res, next) => {
-    if (req.session.sigId && req.url == "/petition") {
-        res.redirect("/petition/signed");
-    } else {
-        next();
-    }
-});
+middleware(app);
 
 //GET REQUESTS
 app.get("/petition", (req, res) => {
