88c39c9decd7c4ccfca0b3adb2a6f2509ce62176
gselli12 <tgsellmann@gmail.com>
2017-08-26 12:17:00 +0200

    commit bedore pushing redis version to heroku

--- middleware.js
+++ middleware.js
@@ -1,21 +1,24 @@
-//REQUIRING
 
-var session = require('express-session'),
-    Store = require('connect-redis')(session);
 
-let secret;
-if(process.env.COOKIE_SECRET) {
-    secret = process.env.COOKIE_SECRET;
-} else {
-    const secrets = require("./secrets.json");
-    secret = secrets.secret;
-}
+
+
+
 
 
 //MIDDLEWARE
 
 module.exports = (app) => {
 
+    //REQUIRING
+
+    let secret;
+    if(process.env.COOKIE_SECRET) {
+        secret = process.env.COOKIE_SECRET;
+    } else {
+        const secrets = require("./secrets.json");
+        secret = secrets.secret;
+
+    }
 
     app.use(require("cookie-parser")());
 
@@ -23,33 +26,47 @@ module.exports = (app) => {
         extended: false
     }));
 
-    app.use(session({
-        store: new Store({
+    var session = require('express-session'),
+        Store = require('connect-redis')(session);
+
+
+    var store = {};
+    if(process.env.REDIS_URL) {
+        store = {
+            url: process.env.REDIS_URL
+        };
+    } else {
+        store = {
             ttl: 3600,
             host: 'localhost',
-            port: 6379
-        }),
-        resave: false,
+            port: 6379,
+        };
+    }
+
+
+    app.use(session({
+        store: new Store(store),
+        resave: true,
         saveUninitialized: true,
         secret: secret
     }));
 
-    // app.use((req, res, next) => {
-    //     if (req.session.sigId && req.url == "/petition") {
-    //         res.redirect("/petition/signed");
-    //     } else {
-    //         next();
-    //     }
-    // });
-    //
-    // app.use((req, res, next) => {
-    //     if( !req.session.id && req.url !== "/register" && req.url !== "/login") {
-    //         res.redirect("/register");
-    //     } else if (req.session.id && (req.url == "/register" || req.url == "/login")) {
-    //         res.redirect("/petition");
-    //     } else {
-    //         next();
-    //     }
-    // });
+    app.use((req, res, next) => {
+        if (req.session.sigId && req.url == "/petition") {
+            res.redirect("/petition/signed");
+        } else {
+            next();
+        }
+    });
+
+    app.use((req, res, next) => {
+        if( !req.session.id && req.url !== "/register" && req.url !== "/login") {
+            res.redirect("/register");
+        } else if (req.session.id && (req.url == "/register" || req.url == "/login")) {
+            res.redirect("/petition");
+        } else {
+            next();
+        }
+    });
 
 };
